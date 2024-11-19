b87325bf0dc9a2ce70423a65a90d0909e629e83b
0xmarshall <0xmarshall@protonmail.com>
2024-11-19 18:28:17 +0000

    fix(hermes-v2-sdk): refactor aprPercentage function to take into account minimum and max boost

--- sdks/hermes-v2-sdk/src/utils/aprPercentage.ts
+++ sdks/hermes-v2-sdk/src/utils/aprPercentage.ts
@@ -1,4 +1,4 @@
-import { YEAR } from '../constants/constants'
+import { YEAR as YEAR_IN_SECONDS } from '../constants/constants'
 
 type Row = {
   tokenPriceUSD: number
@@ -10,25 +10,21 @@ type Row = {
 }
 
 export function aprPercentage(row: Row): string {
+  if (row.tokenPriceUSD == 0) return '0% - 0%'
+
+  // 31536000 = 365 days in seconds
+  const multiplier = (YEAR_IN_SECONDS / (row.endTime - row.startTime)) * 100
+  const rewardsUSD = row.reward * row.tokenPriceUSD
+
   const firstPercentage =
-    (row.tokenPriceUSD > 0 &&
-      row.fullRangeLiquidityUSD > 0 &&
-      (
-        ((row.reward * row.tokenPriceUSD) / row.fullRangeLiquidityUSD) *
-        (YEAR / (row.endTime - row.startTime)) *
-        100
-      ).toFixed(2)) ||
-    '0'
+    (row.fullRangeLiquidityUSD > 0 && (apr(rewardsUSD, row.fullRangeLiquidityUSD, multiplier) * 0.4).toFixed(2)) || '0'
 
   const secondPercentage =
-    (row.tokenPriceUSD > 0 &&
-      row.activeLiquidityUSD > 0 &&
-      (
-        ((row.reward * row.tokenPriceUSD) / row.activeLiquidityUSD) *
-        (YEAR / (row.endTime - row.startTime)) *
-        100
-      ).toFixed(2)) ||
-    '0'
+    (row.activeLiquidityUSD > 0 && apr(rewardsUSD, row.activeLiquidityUSD, multiplier).toFixed(2)) || '0'
 
   return `${firstPercentage}% - ${secondPercentage}%`
+}
+
+function apr(rewardsUSD: number, liquidityUSD: number, multiplier: number): number {
+  return (rewardsUSD / liquidityUSD) * multiplier
 }
