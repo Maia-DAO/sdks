8587f4b41e2be7ac0e0912fc43ef595ccc784a7b
0xBugsy <0xbuggy2@proton.me>
2025-03-24 15:59:22 +0000

    fix(hermes-and-ulysses): Update maia-core

--- sdks/ulysses-sdk/src/constants/index.ts
+++ sdks/ulysses-sdk/src/constants/index.ts
@@ -16,6 +16,7 @@ export const EVM_CHAIN_ID_TO_ROOT_CHAIN_ID: { [key in SupportedChainId]: Support
   [SupportedChainId.METIS]: SupportedChainId.ARBITRUM_ONE,
   [SupportedChainId.BASE]: SupportedChainId.ARBITRUM_ONE,
   [SupportedChainId.SONIC]: SupportedChainId.ARBITRUM_ONE,
+  [SupportedChainId.BERA]: SupportedChainId.ARBITRUM_ONE,
 
   // [SupportedChainId.FANTOM]: SupportedChainId.ARBITRUM_ONE,
   // [SupportedChainId.SCROLL]: SupportedChainId.ARBITRUM_ONE,
@@ -215,6 +216,10 @@ export const DEFAULT_GAS_PARAMS: gasParamsMap = {
   [SupportedChainId.SONIC]: {
     gasLimit: JSBI.BigInt(600000).toString(),
     remoteBranchExecutionGas: JSBI.BigInt(1e18).toString(),
+  },
+  [SupportedChainId.BERA]: {
+    gasLimit: JSBI.BigInt(600000).toString(),
+    remoteBranchExecutionGas: JSBI.BigInt(1e17).toString(),
   },
 
   // Testnets
