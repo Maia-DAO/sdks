bc69ece4801cd7233a14ad93116b4ed676f8300d
0xBugsy <0xbuggy2@proton.me>
2025-04-18 11:17:54 +0100

    fix(core-sdk): update token hierarchy to enable typescript narrowing for globalTokens

--- sdks/core-sdk/src/entities/tokens/nativeCurrency/index.ts
+++ sdks/core-sdk/src/entities/tokens/nativeCurrency/index.ts
@@ -3,7 +3,7 @@ import { BaseCurrency } from '../baseCurrency'
 /**
  * Represents the native currency of the chain on which it resides, e.g. eth on Mainnet
  */
-export abstract class NativeCurrency extends BaseCurrency<false> {
+export abstract class NativeCurrency extends BaseCurrency {
   public readonly isNative: true = true as const
   public readonly isToken: false = false as const
 
