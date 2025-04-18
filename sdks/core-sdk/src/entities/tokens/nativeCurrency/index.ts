eed2c07cd1bb182591a1d45b26b0cf98865a6db8
0xBugsy <0xbuggy2@proton.me>
2025-04-18 12:15:59 +0100

    fix(core-sdk): bump

--- sdks/core-sdk/src/entities/tokens/nativeCurrency/index.ts
+++ sdks/core-sdk/src/entities/tokens/nativeCurrency/index.ts
@@ -1,7 +1,7 @@
 import { BaseCurrency } from '../baseCurrency'
 
 /**
- * Represents the native currency of the chain on which it resides, e.g. eth on Mainnet
+ * Represents the native currency of the chain on which it resides, e.g. eth on Mainnet.
  */
 export abstract class NativeCurrency extends BaseCurrency {
   public readonly isNative: true = true as const
