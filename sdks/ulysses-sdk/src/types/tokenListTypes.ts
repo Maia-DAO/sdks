0bdc1e6ac18c48833c2013a05b714e73b73b8837
0xBugsy <0xbuggy2@proton.me>
2025-10-06 11:27:39 +0100

    fix(ulysses-sdk): update token list types

--- sdks/ulysses-sdk/src/types/tokenListTypes.ts
+++ sdks/ulysses-sdk/src/types/tokenListTypes.ts
@@ -3,29 +3,7 @@ type ExtensionValue = string | number | boolean | null | undefined
 type Extensions = {
   readonly [key: string]: ExtensionValue | Extensions
 }
-
 export interface TokenInfo {
-  readonly chainId: number
-  readonly globalAddress: string
-  readonly localAddress?: { [key: number]: string }
-  readonly underlyingAddress: string
-  readonly name: string
-  readonly decimals: number
-  readonly symbol: string
-  readonly logoURI?: string
-  readonly tags?: string[]
-  readonly extensions?: Extensions
-  readonly isAcross?: boolean
-  readonly isOFT?: boolean
-  readonly oftAdapter?: string
-  readonly oftVersion?: number
-  readonly endpointVersion?: number
-  readonly endpointId?: number
-  readonly oftSharedDecimals?: number
-  readonly noLiquidityOnChain?: boolean
-}
-
-export interface RootTokenInfo {
   readonly chainId: number
   readonly address: string
   readonly name: string
@@ -34,14 +12,6 @@ export interface RootTokenInfo {
   readonly logoURI?: string
   readonly tags?: string[]
   readonly extensions?: Extensions
-  readonly isAcross?: boolean
-  readonly isOFT?: boolean
-  readonly oftAdapter?: string
-  readonly oftVersion?: number
-  readonly endpointVersion?: number
-  readonly endpointId?: number
-  readonly oftSharedDecimals?: number
-  readonly noLiquidityOnChain?: boolean
 }
 
 export interface Version {
@@ -62,20 +32,6 @@ export interface TokenList {
   readonly timestamp: string
   readonly version: Version
   readonly tokens: TokenInfo[]
-  readonly rootTokens: RootTokenInfo[]
-  readonly keywords?: string[]
-  readonly tags?: Tags
-  readonly logoURI?: string
-}
-
-export interface RootTokenList {
-  readonly name: string
-  readonly timestamp: string
-  readonly version: Version
-  readonly tokens: RootTokenInfo[]
-  readonly tokenMap?: {
-    readonly [key: string]: TokenInfo
-  }
   readonly keywords?: string[]
   readonly tags?: Tags
   readonly logoURI?: string
