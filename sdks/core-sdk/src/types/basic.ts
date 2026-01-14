95640c8f4d4bb41973485dd1c2d8a802cc0e8486
0xBugsy <0xbuggy2@proton.me>
2026-01-14 12:20:07 +0000

    fix(core-sdk): remove unnecessary periods in comments

--- sdks/core-sdk/src/types/basic.ts
+++ sdks/core-sdk/src/types/basic.ts
@@ -1,11 +1,11 @@
 import { SupportedChainId } from '../constants'
 
-// Define the basic protocol addresses type.
+// Define the basic protocol addresses type
 export interface IProtocolAddresses {
   [contractName: string]: string
 }
 
-// Define the basic chain ID to layerzero chain ID map type or vice versa.
+// Define the basic chain ID to layerzero chain ID map type or vice versa
 export type chainIdMap = { [chainId: number]: number }
 
 export type ProtocolAddressesType<T> = {
